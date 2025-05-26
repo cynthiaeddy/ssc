3395f2e7ae3768d18f912a63798556a27fb0c34b
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-05-25 21:40:04 -0400

    deco box thinner lines, max width

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,15 +7,14 @@ export const Home = () => {
   return (
 
     <div className='HomeContainer'>
-      <h1>Welcome to the Home Page</h1>
-      <div className='box'></div>
          <section className="fancy-box">
         <div className="corner top-left"></div>
         <div className="corner top-right"></div>
         <div className="corner bottom-left"></div>
         <div className="corner bottom-right"></div>
-        <div className="box-content">Box Content Here</div>
+        <div className="box-content type" >We make eco-friendly, artisanal soaps and lotions that blend effectiveness with environmental responsibility, delivering sustainable products that you will love.</div>
       </section>
+      <div className='box'></div>
       </div>
 
   )
