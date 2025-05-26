894eab41db1bf6e2363ec61e87ca8dd879b0da31
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-05-25 21:08:30 -0400

    deco box created

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -9,6 +9,13 @@ export const Home = () => {
     <div className='HomeContainer'>
       <h1>Welcome to the Home Page</h1>
       <div className='box'></div>
+         <section className="fancy-box">
+        <div className="corner top-left"></div>
+        <div className="corner top-right"></div>
+        <div className="corner bottom-left"></div>
+        <div className="corner bottom-right"></div>
+        <div className="box-content">Box Content Here</div>
+      </section>
       </div>
 
   )
